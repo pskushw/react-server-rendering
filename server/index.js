99ca35a5a8de197145660a012c8bad1df55558f6
Priyanka Singh <nidhibsp.singh@gmail.com>
2020-10-21 12:08:28 +0530

    Revert "first draft"
    This reverts commit 5e01daf474670f0bdb3826cf587460927606a4d4.

--- server/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = new express();                                          
-
-app.get('/', (_req, res) => {
-    res.send(`<h1>Hello world</h1>`);
-});
-
-app.listen(8000);
-console.log("Server is listenling");
